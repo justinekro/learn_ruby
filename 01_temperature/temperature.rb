feee6dd269e6ab74f2547ff75029865ba26b084d
Justine <>
2018-01-16 19:46:05 +0100

    exo 4 ok

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,9 +1,9 @@
 def ftoc(far)
-  far = (far - 32) * 5.0/9.0
+  far = ((far - 32) * 5.0/9.0).round(2)
 end
 
 
 def ctof(cel)
-  cel = cel * 9.0/5.0 + 32 
+  cel = (cel * 9.0/5.0 + 32).round(2)
 end
 
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
-# write your code here
+
+  attr_writer :booktitle
+
+def title
+  booktitle = :booktitle
+  result = booktitle.split.map! { |string| 
+  	if (string == "and" || string == "the" || string == "over")
+  	string 
+    else 
+  	string.capitalize!
+    end
+
+  }
+  result[0].capitalize!
+  result.join(" ")
+
+ end
+
+
 end
+
+new_book = Book.new
+new_book.booktitle = "salut les copains"
+
+print new_book.title
